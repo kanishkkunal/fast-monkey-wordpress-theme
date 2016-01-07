66d772136d09a34231ba3dc951a31e8eb9f253cd
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-01-07 16:05:26 +0530

    Removing unnecessary code

--- index.php
+++ index.php
@@ -4,32 +4,11 @@ beans_add_smart_action( 'beans_before_load_document', 'fast_monkey_index_setup_d
 
 function fast_monkey_index_setup_document() {
 
-	// Posts grid
-	beans_add_attribute( 'beans_content', 'class', 'tm-posts-grid' );
-	beans_wrap_inner_markup( 'beans_post', 'fast_monkey_post_panel', 'div', array(
-	  'class' => 'uk-panel uk-panel-box'
-	) );
-
-	// Post content
-	beans_remove_attribute( 'beans_content', 'class', 'tm-centered-content' );
-
-	// Post article
-	beans_remove_attribute( 'beans_post', 'class', 'uk-article' );
-
 	// Post meta
 	beans_remove_action( 'beans_post_meta_tags' );
 
-	// Post image
-	beans_modify_action( 'beans_post_image', 'beans_post_header_before_markup', 'beans_post_image' );
-
-	// Post title
-	beans_add_attribute( 'beans_post_title', 'class', 'uk-margin-small-top uk-h2' );
-
 	// Post more link
 	beans_add_attribute( 'beans_post_more_link', 'class', 'uk-button uk-button-primary uk-button-small' );
-
-	// Posts pagination
-	beans_modify_action_hook( 'beans_posts_pagination', 'beans_content_after_markup' );
 
 }
 
@@ -54,15 +33,5 @@ function fast_monkey_post_content( $content ) {
 
 }
 
-// Resize post image (filter)
-beans_add_smart_action( 'beans_edit_post_image_args', 'fast_monkey_index_post_image_args' );
-
-function fast_monkey_index_post_image_args( $args ) {
-
-	$args['resize'] = array( 756, 320, true );
-
-	return $args;
-
-}
 // Load beans document
 beans_load_document();
